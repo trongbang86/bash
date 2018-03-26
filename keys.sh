b570c7a763f6bde692c6917093a1f0d989117b46
BangNguyen <Bang.Nguyen@macquarie.com>
2018-03-26 11:43:31 +1100

    added unzip.tar

--- keys.sh
+++ keys.sh
@@ -92,3 +92,55 @@ function ps.grep() {
         sed $'s/-D/\\\n-D/g' | \
         less
 }
+
+# this unzips a tar file and opens the extracted folder for checking
+# 1) read the file name
+# 2) check if it exists
+# 3) check if $TMP exists
+# 4) unzip
+# 5) open folder
+# 6) go back to the previous folder
+function unzip.tar() {
+    # 1)
+    echo 'Reading file name'
+    file=$1
+    CURR=`pwd`
+    if [ -z "$file" ]; then
+        read -p "Enter a file :" file
+    fi
+    # 2)
+    echo 'Checking file existence'
+    if [ ! -f "$file" ]; then
+        echo File $file does not exist
+        return 1
+    fi
+    # 3)
+    echo 'Checking if $TMP exists'
+    if [ -z "$TMP" ]; then
+        echo 'Please set $TMP'
+        return 1
+    fi
+    # 4)
+    echo 'unzipping'
+    fileName=$(basename "$file")
+    folder=$TMP/unzip
+    if [ -f "$folder" ]; then
+        echo "$folder should be a folder"
+        return 1
+    fi
+    rm -rf $folder
+    mkdir $folder
+    cp $file $folder
+    cd $folder
+    tar -xzf $fileName
+    rm $fileName
+    # 5)
+    echo 'Openning zipped folder'
+    open .
+    # 6)
+    cd $CURR
+    unset fileName
+    unset folder
+    unset CURR
+    unset file
+}
